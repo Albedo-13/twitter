9acab0ac67d15cd3fadb9e2d841955a789f697f4
PavelProkopenya <prokopenya.personal@gmail.com>
2024-05-03 10:03:43 +0300

    init: initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@types": path.resolve(__dirname, "./src/types/index.ts"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@mocks": path.resolve(__dirname, "./src/mocks"),
+    },
+  },
   plugins: [react()],
-})
+});
