4403b459a234773dc3fec347602ec2399525142a
PavelProkopenya <prokopenya.personal@gmail.com>
2024-05-20 12:05:10 +0300

    feat: redirect on post id error

--- src/pages/post-page/post-page.tsx
+++ src/pages/post-page/post-page.tsx
@@ -6,8 +6,9 @@ import {
   where,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
+import { ToggleTheme } from "@/components/toggle-theme/toggle-theme";
 import { Tweet } from "@/components/tweet/tweet";
 import { db } from "@/firebase";
 import { useAppSelector } from "@/hooks/redux";
@@ -17,6 +18,7 @@ export function PostPage() {
   const [post, setPost] = useState<DocumentData | null>(null);
   const user = useAppSelector(getUserSelector);
 
+  const navigate = useNavigate();
   const location = useLocation();
   const postUid = location.pathname.split("/").at(-1);
 
@@ -24,14 +26,20 @@ export function PostPage() {
     const querySnapshot = await getDocs(
       query(collection(db, "posts"), where("uid", "==", postUid))
     );
+    if (!querySnapshot.docs[0]) throw new Error("no post by this id");
     const post = querySnapshot.docs[0].data();
     setPost(post);
   };
 
   useEffect(() => {
-    getPostByUid();
+    getPostByUid().catch(() => navigate("/"));
   }, []);
 
   // TODO: loader
-  return post ? <Tweet userUid={user.uid} post={post} /> : <p>loading...</p>;
+  return (
+    <>
+      <ToggleTheme />
+      {post ? <Tweet userUid={user.uid} post={post} /> : <p>loading...</p>}
+    </>
+  );
 }
