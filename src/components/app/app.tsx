7934c32f622f3ecc90ba2992ba73aa04a5c88fb2
NovaPride <novapride27@gmail.com>
2024-10-01 14:20:45 +0300

    add: account information confirmation toast

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,6 +7,7 @@ import { GlobalStyle } from "@/styles/global";
 import { themeDark, themeLight } from "@/styles/theme";
 
 import { router } from "./router";
+import { ToastContainer } from "react-toastify";
 
 export function App() {
   const theme = useAppSelector(getThemeSelector);
@@ -15,6 +16,18 @@ export function App() {
     <ThemeProvider theme={theme === "light" ? themeLight : themeDark}>
       <GlobalStyle />
       <RouterProvider router={router} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </ThemeProvider>
   );
 }
